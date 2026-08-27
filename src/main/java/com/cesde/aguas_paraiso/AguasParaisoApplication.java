d9b7cf1eea6eab728d2ea4917de715329add9972
Guelo708 <mguelo7@gmail.com>
2026-08-27 00:07:17 -0500

    Creacion del proyecto aguas paraiso

---
+++ src/main/java/com/cesde/aguas_paraiso/AguasParaisoApplication.java
@@ -0,0 +1,13 @@
+package com.cesde.aguas_paraiso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AguasParaisoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AguasParaisoApplication.class, args);
+	}
+
+}
