f289964e64e1dc3504acab2b3263240b87c0d20c
Miguel Eduardo Hernández Hernández <mguelo7@gmail.com>
2026-09-16 12:55:13 -0500

    refactor: eliminar relacion many to many entre usuario y sector

--- src/main/java/com/cesde/aguas_paraiso/model/entity/Usuario.java
+++ src/main/java/com/cesde/aguas_paraiso/model/entity/Usuario.java
@@ -33,18 +33,8 @@ public class Usuario extends BaseEntity {
     @Column(length = 20)
     private String celular;
 
-    // Relación con Predios (OneToMany)
     @OneToMany(mappedBy = "propietario")
     private List<Predio> predios;
-
-    // Relación con Sectores (ManyToMany)
-    @ManyToMany
-    @JoinTable(
-        name = "usuario_sector",
-        joinColumns = @JoinColumn(name = "usuario_id"),
-        inverseJoinColumns = @JoinColumn(name = "sector_id")
-    )
-    private List<Sector> sectores;
 }
 
 
